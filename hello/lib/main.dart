1f754d439bdef4f882d1a8349d8a1f1400606c6e
angcon175 <angcon26011998@gmail.com>
2023-06-21 02:06:35 +0600

    ..

---
+++ hello/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text("Hello World")))));
+}
